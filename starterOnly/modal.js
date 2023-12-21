8387dc02af265e97474b06027444e024083e34c0
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-12-21 13:41:26 +0100

    fermer la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,7 +8,7 @@ function editNav() {
 }
 
 // DOM Elements
-// const form = document.querySelector("#form");
+const form = document.querySelector("#form");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
@@ -21,49 +21,76 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+
+
+
+
+// Fermer la modal
+const modalbgBis = document.querySelector(".bgroundBis");
+var span0 = document.getElementsByClassName("close")[0]; //premier élément de classe "close"
+var span1 = document.getElementsByClassName("close")[1]; //deuxième élément de classe "close"
+const modalForm = document.getElementById("myForm");
+
+span0.onclick = function () {
+  modalbgBis.style.display = "none";
+  modalbg.style.display = "none";
+}
+span1.onclick = function () {
+  modalForm.reset(); //réinitialise tous les champs du formulaire à leurs valeurs par défaut.
+  modalbgBis.style.display = "none";
+  modalbg.style.display = "none";
+}
+// modalForm.addEventListener('submit', validate);
+
+
+
+
+
 form.addEventListener("submit", validate)
 
 function validate(event) {
   event.preventDefault()
   console.log('C\'est la validation du formulaire')
 
+  
+
 
 
   // Définition et récupération de tous les élements dans des variables 
-const form = document.getElementById('myForm');
-const firstName = document.getElementById('first');
-const lastName = document.getElementById('last');
-const mail = document.getElementById('email');
-const birthdate = document.getElementById('birthdate');
-const quantity = document.getElementById('quantity');
-   //je définis mes variables pour récupérer les valeurs saisies 
+  const form = document.getElementById('myForm');
+  const firstName = document.getElementById('first');
+  const lastName = document.getElementById('last');
+  const mail = document.getElementById('email');
+  const birthdate = document.getElementById('birthdate');
+  const quantity = document.getElementById('quantity');
+
+  //je définis mes variables pour récupérer les valeurs saisies 
   //et j'utilise la fonction trim pour retirer les espaces en début et fin de saisie 
-  const firstNameValue = firstName.value.trim();
+  const firstNameValue = firstName.value.trim(); // trim supprime les espaces aux 2 extrémités de la chaîne et renvoie une nouvelle chaîne, sans modifier la chaîne d'origine.
   const lastNameValue = lastName.value.trim();
   const mailValue = mail.value.trim();
   const birthdateValue = birthdate.value.trim();
   const quantityValue = quantity.value.trim();
-  
+
   //Je définis les conditions d'erreur et affiche le ou les messages associé
-  if(firstNameValue == "") {
+  if (firstNameValue == "") {
     console.log('Le prénom est requis');
   } else if (firstNameValue.length < 2) {
     console.log("Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
   } else {
     console.log(firstName);
   }
- 
-  if(lastNameValue == "") {
+
+  if (lastNameValue == "") {
     console.log('Le nom est requis');
   } else if (lastNameValue.length < 2) {
     console.log("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
   } else {
     console.log(lastName);
   }
-  
-  if(mailValue == "") {
+
+  if (mailValue == "") {
     console.log('L\'adresse email est requise');
-    //ici je teste si les valeurs saisies sont conformes à ma REGEX défini plus haut
   } else if (mailValid.test(mailValue) == false) {
     //si non conforme j'affiche ce message
     console.log("Votre adresse mail n\'a pas le bon format.")
@@ -71,20 +98,20 @@ const quantity = document.getElementById('quantity');
     // si conforme et que le champ n'est pas vide alors j'affiche le succès 
     console.log(mail);
   }
-  
+
   //axe d'amélioration
-  if(birthdateValue == "") {
+  if (birthdateValue == "") {
     console.log("Vous devez entrer votre date de naissance.");
   } else {
     console.log(birthdate);
   }
-  
-  if(quantityValue == "" || quantityValue < 0) {
+
+  if (quantityValue == "" || quantityValue < 0) {
     console.log('Veuillez saisir un chiffre');
   } else {
     console.log(quantity);
   }
 
 
-  }
+}
 
